fa76f011c71a13de976369c977b98636c47db620
Zahra <zahradeenie@gmail.com>
2020-12-08 17:36:07 +0100

    fix: update prod config url host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :bernous_cv, BernousCvWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "pure-ocean-58174.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "bernous.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
