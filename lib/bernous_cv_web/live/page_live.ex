c7ee8fd42edd8698d9d7543e420250c22812843d
Zahra <zahradeenie@gmail.com>
2020-12-08 11:41:28 +0100

    feat: add button + / - functionality

--- lib/bernous_cv_web/live/page_live.ex
+++ lib/bernous_cv_web/live/page_live.ex
@@ -3,37 +3,18 @@ defmodule BernousCvWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, :gluten, 20)}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("increase", _, socket) do
+    socket = update(socket, :gluten, &min(&1 + 10, 100))
+    {:noreply, socket}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not BernousCvWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  def handle_event("decrease", _, socket) do
+    socket = update(socket, :gluten, &max(&1 - 10, 0))
+    {:noreply, socket}
   end
 end
